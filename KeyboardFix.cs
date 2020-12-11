c682e9dd61d2ff5cc2e5722be74ac7d70c15613d
Tommy <tommy.stanley96@gmail.com>
2020-12-10 18:42:02 -0600

    Inital Commit

---
+++ KeyboardFix.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using BepInEx;
+using Rewired;
+
+namespace CloneHeroMod
+{
+    [BepInPlugin("com.tommysource.plugins.keyboardfix", "Keyboard Fix", "1.0")]
+    public class KeyboardFix : BaseUnityPlugin
+    {
+        private void Start()
+        {
+            foreach (Player p in ReInput.players.AllPlayers)
+            {
+                p.controllers.maps.SetAllMapsEnabled(true);
+            }
+        }
+    }
+}
